7bde53a29a545febafb43670707f80ab173cbe5f
André Morales <an.dre.m@outlook.com>
2024-02-22 11:10:43 -0300

    refact: disable leftover apps
    refact: Disable old apps and move them to the untracked .old folder.

--- client/res/js/client_core.js
+++ client/res/js/client_core.js
@@ -53,7 +53,7 @@ async function main() {
 
 class ClientClass {
 	constructor() {
-		this.CLIENT_VERSION = '1.0.117';
+		this.CLIENT_VERSION = '1.0.118';
 		this.BUILD_STRING = `${this.CLIENT_VERSION} Early Test 1`
 		this.BUILD_TEXT = `Clouds ${this.BUILD_STRING}`;
 	}
