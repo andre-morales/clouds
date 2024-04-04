a6498e6d73500544ab8f5b5e1150936186343f2f
André Morales <an.dre.m@outlook.com>
2024-04-04 18:20:56 -0300

    build: separated webpack config files

--- config/webpack/dev.cjs
+++ config/webpack/dev.cjs
@@ -1,51 +1,6 @@
-const path = require('path');
-const ROOT = path.resolve(__dirname, '../');
+const master = require('./master.cjs');
 
-module.exports = {
-	mode: 'development',
-	devtool: 'eval',
-	entry: {
-		shared: {
-			import: './client/src/client_core.mjs',
-			library: {
-				name: 'CoreModule',
-				type: 'umd'
-			}
-		},
-		public: {
-			import: './client/src/public.mjs',
-			dependOn: 'shared',
-			library: {
-				name: 'PublicModules',
-				type: 'umd'
-			}
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(?:js|mjs|cjs)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
-						presets: [
-							['@babel/preset-env', {
-								targets: {
-									chrome: 79
-								},
-								useBuiltIns: 'usage',
-								corejs: '^3.36.1'
-							}]
-						]
-					}
-				}
-			}
-		]
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(ROOT, 'client/res/pack'),
-	}
-};
+master.mode = 'development';
+master.devtool = 'eval';
+
+module.exports = master;
---
+++ config/webpack/master.cjs
@@ -0,0 +1,51 @@
+const path = require('path');
+const ROOT = path.resolve(__dirname, '../');
+
+module.exports = {
+	mode: 'production',
+	devtool: 'source-map',
+	entry: {
+		shared: {
+			import: './client/src/client_core.mjs',
+			library: {
+				name: 'CoreModule',
+				type: 'umd'
+			}
+		},
+		public: {
+			import: './client/src/public.mjs',
+			dependOn: 'shared',
+			library: {
+				name: 'PublicModules',
+				type: 'umd'
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true,
+						presets: [
+							['@babel/preset-env', {
+								targets: {
+									chrome: 79
+								},
+								useBuiltIns: 'usage',
+								corejs: '^3.36.1'
+							}]
+						]
+					}
+				}
+			}
+		]
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(ROOT, 'client/res/pack'),
+	}
+};
