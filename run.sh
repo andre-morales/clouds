bb23c166c5715ec976079df802343ba7fc0854a8
André Morales <an.dre.m@outlook.com>
2025-09-09 17:12:47 +0000

    build: updated run scripts to use improved envs

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ if [ ! -f "./apps/about/dist/app.pack.js" ]; then
     pnpm apps:build:prod
 fi
 
-node --enable-source-maps --watch-path api/dist . $@
+node --env-file envs/prod.env --enable-source-maps --watch-path api/dist . $@
